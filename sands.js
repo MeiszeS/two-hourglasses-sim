dfebfc6c5520665a12d0a2f2e6d9bc4fd4149f50
MeiszeS <imelena069@gmail.com>
2026-06-26 17:34:12 -0700

    Added hourglass emptying and display

--- sands.js
+++ sands.js
@@ -4,22 +4,36 @@ const sands = hourglass.getContext("2d");
 hourglass.height = 1000;
 hourglass.width = 2000;
 
-var capacityA = 4;
-var capacityB = 7;
+let critical = false;
+
+var capacityA = 37;
+var capacityB = 56;
 var elapsedA = 0;
 var elapsedB = 0;
 
 function drawHourglass(originX, originY, width, height, theta, max, elapsed){
+    let topA = Math.sqrt((max-elapsed)/max);
+    let bottomA = Math.sqrt(elapsed/max);
+
+    //Rotation
     sands.translate(originX, originY);
     sands.rotate(theta);
     sands.translate(-originX, -originY);
 
-    console.log(elapsed/max);
+    console.log(topA);
 
     //Top Fill
     sands.beginPath();
-    sands.moveTo(originX-(elapsed/max)*(width/2), originY-(elapsed/max)*(height/2));
-    sands.lineTo(originX+(elapsed/max)*(width/2), originY-(elapsed/max)*(height/2));
+    sands.moveTo(originX-topA*(width/2), originY-topA*(height/2));
+    sands.lineTo(originX+topA*(width/2), originY-topA*(height/2));
+    sands.lineTo(originX, originY);
+    sands.closePath();
+    sands.fill();
+
+    //Bottom Fill
+    sands.beginPath();
+    sands.moveTo(originX-bottomA*(width/2), originY+bottomA*(height/2));
+    sands.lineTo(originX+bottomA*(width/2), originY+bottomA*(height/2));
     sands.lineTo(originX, originY);
     sands.closePath();
     sands.fill();
@@ -32,6 +46,7 @@ function drawHourglass(originX, originY, width, height, theta, max, elapsed){
     sands.closePath()
     sands.stroke()
 
+    //Bottom Outline
     sands.beginPath()
     sands.moveTo(originX-width/2, originY+height/2);
     sands.lineTo(originX+width/2, originY+height/2);
@@ -39,6 +54,7 @@ function drawHourglass(originX, originY, width, height, theta, max, elapsed){
     sands.closePath()
     sands.stroke()
 
+    //Reset rotation
     sands.setTransform(1, 0, 0, 1, 0, 0);
 }
 
@@ -46,11 +62,21 @@ var t = 0;
 function drawFrame(){
     sands.lineWidth = 10;
     sands.fillStyle = "green";
+    sands.font = "50px serif";
     sands.clearRect(0, 0, 2000, 1000);
-    drawHourglass(600, 500, 300, 700, 0, 100, 101);
-    //drawHourglass(1400, 500, 300, 700, t/110);
+
+    drawHourglass(600, 500, 300, 700, 0, capacityA, t);
+    drawHourglass(1400, 500, 300, 700, 0, capacityB, t);
+
+    //Draw time interface
+    sands.fillText("Remaining: "+(capacityA-t), 100, 300);
+    sands.fillText("Elapsed: "+(elapsedA), 100, 700);
+    sands.fillText("Remaining: "+(capacityB-t), 1600, 300);
+    sands.fillText("Elapsed: "+(elapsedB), 1600, 700);
+    elapsedA++;
+    elapsedB++;
     t++;
 }
 
-setInterval(drawFrame, 25);
+setInterval(drawFrame, 1000);
 
