0292134093dbd6482bb7dad47813eab0d8125a35
MeiszeS <imelena069@gmail.com>
2026-06-26 21:42:17 -0700

    First functional version, without customization or good animation

--- sands.js
+++ sands.js
@@ -1,13 +1,23 @@
 const hourglass = document.getElementById("hourglass");
 const sands = hourglass.getContext("2d");
 
+const stopwatch = document.getElementById("stopwatch")
+const time = stopwatch.getContext("2d");
+
+const flipA = document.getElementById("flipA");
+const flipBoth = document.getElementById("flipBoth")
+const flipB = document.getElementById("flipB");
+
 hourglass.height = 1000;
 hourglass.width = 2000;
 
+let finishedA = false;
+let finishedB = false;
 let critical = false;
 
-var capacityA = 37;
-var capacityB = 56;
+var t = 0;
+var capacityA = 4;
+var capacityB = 7;
 var elapsedA = 0;
 var elapsedB = 0;
 
@@ -20,7 +30,7 @@ function drawHourglass(originX, originY, width, height, theta, max, elapsed){
     sands.rotate(theta);
     sands.translate(-originX, -originY);
 
-    console.log(topA);
+    //console.log(topA);
 
     //Top Fill
     sands.beginPath();
@@ -58,25 +68,81 @@ function drawHourglass(originX, originY, width, height, theta, max, elapsed){
     sands.setTransform(1, 0, 0, 1, 0, 0);
 }
 
-var t = 0;
-function drawFrame(){
+function drawFrame(rotationA = 0, rotationB = 0){
     sands.lineWidth = 10;
     sands.fillStyle = "green";
     sands.font = "50px serif";
     sands.clearRect(0, 0, 2000, 1000);
 
-    drawHourglass(600, 500, 300, 700, 0, capacityA, t);
-    drawHourglass(1400, 500, 300, 700, 0, capacityB, t);
+    drawHourglass(600, 500, 300, 700, rotationA, capacityA, elapsedA);
+    drawHourglass(1400, 500, 300, 700, rotationB, capacityB, elapsedB);
 
     //Draw time interface
-    sands.fillText("Remaining: "+(capacityA-t), 100, 300);
+    sands.fillText("Remaining: "+(capacityA-elapsedA), 100, 300);
     sands.fillText("Elapsed: "+(elapsedA), 100, 700);
-    sands.fillText("Remaining: "+(capacityB-t), 1600, 300);
+    sands.fillText("Remaining: "+(capacityB-elapsedB), 1600, 300);
     sands.fillText("Elapsed: "+(elapsedB), 1600, 700);
-    elapsedA++;
-    elapsedB++;
-    t++;
+
+    //Draw time
+    time.clearRect(0, 0, 1000, 1000);
+    time.font = "80px serif";
+    time.fillStyle = "red";
+    time.fillText(t, 140, 100);
 }
 
-setInterval(drawFrame, 1000);
+function logicLoop(){
+    //Check if at a critical point where user input is needed
+    if(critical){
+        //Do not let time pass until the user makes their decision
+        return;
+    }
+    else{
+        //Let 1 second pass
+        if(!finishedA){elapsedA++;}
+        if(!finishedB){elapsedB++;}
+        t++;
+        drawFrame()
+        if(elapsedA == capacityA && !finishedA){
+            finishedA = true;
+            critical = true;
+        }
+        if(elapsedB == capacityB && !finishedB){
+            finishedB = true;
+            critical = true;
+        }
+    }
+}
+
+function flip(){
+    if(critical){
+        //Play hourglass flip animation then swap top and bottom volumes
+        if(event.target.id == "flipA"){
+                elapsedA = capacityA-elapsedA;
+
+                finishedA = false;
+                critical = false;
+        }
+        if(event.target.id == "flipB"){
+            elapsedB = capacityB-elapsedB;
+
+            finishedB = false;
+            critical = false;
+        }
+        if(event.target.id == "flipBoth"){
+            elapsedA = capacityA-elapsedA;
+            elapsedB = capacityB-elapsedB;
+
+            finishedA = false;
+            finishedB = false;
+            critical = false;
+        }
+        drawFrame();
+    }
+}
+
+drawFrame();
+flipA.addEventListener("click", flip);
+flipB.addEventListener("click", flip);
+flipBoth.addEventListener("click", flip);
+setInterval(logicLoop, 500);
 
